413414d76bd73e2e1e56449ec4446650221c9af3
Kevin Musselman <kmussel@gmail.com>
2019-10-16 15:43:42 -0400

    upgrade tf

--- app/javascript/components/things.js
+++ app/javascript/components/things.js
@@ -44,7 +44,6 @@ export default class Things extends React.Component {
     // tf.io.removeModel(`indexeddb://pluck-model-1`);
     this.loadLatestModel();
     this.loadThings()
-    
   }
 
   loadLatestModel() {
@@ -60,14 +59,14 @@ export default class Things extends React.Component {
 
   async loadModel() {
     this.model = new Promise((resolve, reject) => {
-      var amodel = tf.loadLayersModel(`indexeddb://pluck-model-${this.state.model_version}`);
+      var amodel = tf.loadLayersModel(`indexeddb://pluck-models-${this.state.model_version}`, false);
       amodel.then((data) => {
         resolve(data)
       }).catch((error) => {
         console.log(error)
-        var bmodel = tf.loadLayersModel(`/api/v1/model_versions/${this.state.model_version}/model.json`)
+        var bmodel = tf.loadLayersModel(`/api/v1/model_versions/${this.state.model_version}/model.json`, false)
         bmodel.then((data) => {
-          data.save(`indexeddb://pluck-model-${this.state.model_version}`);
+          data.save(`indexeddb://pluck-models-${this.state.model_version}`);
         })
         return resolve(bmodel)
       });
