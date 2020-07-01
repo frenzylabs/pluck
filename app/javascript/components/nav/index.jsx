8cf920c5905fce66b06fac407930517db469a967
Kevin Musselman <kmussel@gmail.com>
2020-07-01 17:41:56 +0000

    create latest image

--- app/javascript/components/nav/index.jsx
+++ app/javascript/components/nav/index.jsx
@@ -32,7 +32,7 @@ class TopNav extends React.Component {
   }
 
   UNSAFE_componentWillMount() {
-    this.setState({term: "test2"})
+    this.setState({term: "tada"})
     // if (this.props.term !== this.state.term) {
     //   this.setState({term: this.props.term})
     // }
